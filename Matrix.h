2d87f0a5397e99d299d746e6e341d124666f42db
Андрей Степанов <therain3164@yandex.ru>
2024-04-14 17:50:17 +0300

    ygfgbjfvbnj

--- Matrix.h
+++ Matrix.h
@@ -1,87 +1,84 @@
 #pragma once
+
 #include "Vector.h"
 
 template<typename T>
-class Matrix{
+class Matrix {
 private:
     size_t _size;
-    Vector<T>* _vectors;
+    Vector<T> *_vectors;
     size_t _vec_size;
 public:
-    Matrix(){
+    Matrix() {
         _size = 10;
         _vec_size = 10;
         _vectors = new Vector<T>[_size];
     }
-    Matrix(size_t size){
+
+    Matrix(size_t size) {
         _size = size;
         _vec_size = 10;
         _vectors = new Vector<T>[_size];
     }
 
-    Matrix(size_t size, size_t vec_size){
+    Matrix(size_t size, size_t vec_size) {
         _size = size;
         _vec_size = vec_size;
         _vectors = new Vector<T>[_size];
         for (size_t i = 0; i < _size; i++) _vectors[i] = Vector<T>(_vec_size);
     }
 
-    Matrix(size_t size, size_t vec_size, Vector<T>* vectors){
+    Matrix(size_t size, size_t vec_size, Vector<T> *vectors) {
         _size = size;
         _vec_size = vec_size;
         _vectors = new Vector<T>[_size];
         for (size_t i = 0; i < size; i++) _vectors[i] = vectors[i];
     }
 
-    Vector<T>& operator[](int index) const{
+    Vector<T> &operator[](int index) const {
         return _vectors[index];
     }
 
-    ~Matrix(){
-        delete [] _vectors;
+    ~Matrix() {
+        delete[] _vectors;
     }
 
-    Matrix Transposition(){
+    Matrix Transposition() {
         Matrix res = Matrix(_vec_size, _size);
-        for (size_t i = 0; i < _vec_size; i++){
-            for (size_t j = 0; j < _size; j++){
-                res._vectors[i][j] = _vectors[_size - 1 - j][i];
+        for (size_t i = 0; i < _vec_size; i++) {
+            for (size_t j = 0; j < _size; j++) {
+                res._vectors[i][j] = _vectors[j][i];
             }
         }
         return res;
     }
 
-    Matrix operator+(const Matrix& m)const{
-        if (_size != m._size)
-        {
+    Matrix operator+(const Matrix &m) const {
+        if (_size != m._size) {
             throw "Invalid matrix size!";
         }
         Matrix n = Matrix(m._size);
-        for (size_t i = 0; i < _size; i++)
-        {
+        for (size_t i = 0; i < _size; i++) {
             n[i] = (*this)[i] + m[i];
         }
         return n;
     }
 
-    Matrix operator-(const Matrix& m)const{
-        if (_size != m._size)
-        {
+    Matrix operator-(const Matrix &m) const {
+        if (_size != m._size) {
             throw "Invalid matrix size!";
         }
         Matrix n = Matrix(m._size);
-        for (size_t i = 0; i < _size; i++)
-        {
+        for (size_t i = 0; i < _size; i++) {
             n[i] = (*this)[i] - m[i];
         }
         return n;
     }
 
-    Matrix operator*(const Matrix& m) const {
+    Matrix operator*(const Matrix &m) const {
         if (_vec_size != m._size) {
             throw "Invalid matrix size!";
-        }
-        else {
+        } else {
             Matrix n = Matrix(_size, m._vec_size);
 
             T sum = 0;
@@ -92,23 +89,45 @@ public:
                         sum += _vectors[i][j] * m._vectors[j][t];
                     }
                     n[i][t] = sum;
-                    std::cout << n;
+//                    std::cout << n;
                 }
             }
             return n;
         }
     }
 
+    T determinantNxN(T ) {
+        if (_size != _vec_size) { throw "No Determinant ;)"; }
+        if (_size == 1) return _vectors[0][0];
+        if (_size == 2) {
+            return _vectors[0][0] * _vectors[1][1] - _vectors[0][1] * _vectors[1][0];
+        } else if (_size == 3) {
+            return _vectors[0][0] * (_vectors[1][1] * _vectors[2][2] - _vectors[1][2] * _vectors[2][1]) -
+                   _vectors[0][1] * (_vectors[1][0] * _vectors[2][2] - _vectors[1][2] * _vectors[2][0]) +
+                   _vectors[0][2] * (_vectors[1][0] * _vectors[2][1] - _vectors[1][1] * _vectors[2][0]);
+        } else {
+            T det = 0;
+            for (size_t i = 0; i < _size; ++i) {
+                T submatrix[(_size - 1) * (_size - 1)];
+                size_t index = 0;
+                for (size_t j = _size; j < _size * _size; ++j) {
+                    if (j % _size != i) {
+                        submatrix[index++] = _vectors[j / _size][j % _size];
+                    }
+                }
+                det += (i % 2 == 0 ? 1 : -1) * _vectors[i / _size][i % _size] * determinantNxN(submatrix, _size - 1);
+            }
+            return det;
+        }
+    }
 
 
-
-    friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix){
-        for (size_t i =0; i < matrix._size; i++)
+    friend std::ostream &operator<<(std::ostream &os, const Matrix &matrix) {
+        for (size_t i = 0; i < matrix._size; i++)
             os << matrix._vectors[i];
         os << std::endl;
         return os;
     }
-
 
 
 };
--- main.cpp
+++ main.cpp
@@ -11,7 +11,10 @@ int main(){
     int arr7[1] = {4};
     int arr8[1] = {-2};
     int arr9[1] = {5};
-
+    int arr10[4] = {1, 2, 3, 4};
+    int arr11[4] = {1, 2, 3, 4};
+    int arr12[4] = {1, 2, 3, 4};
+    int arr13[4] = {1, 2, 3, 4};
 
     Vector<int> vector3 = Vector<int>(5, arr3);
     Vector<int> vector4 = Vector<int>(5, arr4);
@@ -22,6 +25,13 @@ int main(){
     Vector<int> vector7 = Vector<int>(1, arr7);
     Vector<int> vector8 = Vector<int>(1, arr8);
     Vector<int> vector9 = Vector<int>(1, arr9);
+    Vector<int> vector10 = Vector<int>(4, arr10);
+    Vector<int> vector11 = Vector<int>(4, arr11);
+    Vector<int> vector12 = Vector<int>(4, arr12);
+    Vector<int> vector13 = Vector<int>(4, arr13);
+
+
+
     Vector<int>* a1 = new Vector<int>[2];
     a1[0] = vector1;
     a1[1] = vector2;
@@ -34,15 +44,22 @@ int main(){
     a3[2] = vector7;
     a3[3] = vector8;
     a3[4] = vector9;
+    Vector<int>* a4 = new Vector<int>[4];
+    a4[0] = vector10;
+    a4[1] = vector11;
+    a4[2] = vector12;
+    a4[3] = vector13;
     Matrix<int> m1 = Matrix<int>(2, 5, a1);
     Matrix<int> m2 = Matrix<int>(2, 5,a2);
     Matrix<int> m3 = Matrix<int>(5, 1,a3);
+    Matrix<int> m4 = Matrix<int>(4, 4,a4);
     try {
 //        std::cout << m3 << std::endl << m3.Transposition() << std::endl << std::endl;
 //        std::cout << m1 - m2 << std::endl;
 //        std::cout << m1 + m2 << std::endl;
-        std::cout << m1 << m3 << m1 * m3 << std::endl;
-//        std::cout << m1 << std::endl << m1.Transposition() << std::endl;
+//        std::cout << m1 << m3 << m1 * m3 << std::endl;
+//        std::cout << m3 << std::endl << m3.Transposition() << std::endl;
+        std::cout << m4.determinantNxN();
     }
     catch (const char* e){
         std::cerr << e << std::endl;
